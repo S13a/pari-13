85a07d2380883fc6ac74afd7e3e4ccd951700198
User <User@DESKTOP-5FLENJ9>
2023-04-07 02:01:00 +0530

    User story number 123 changes done.

---
+++ addition/src/addition/example.java
@@ -0,0 +1,27 @@
+package addition;
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		  // Addition
+		int a = 10, b = 20;
+		System.out.println(10+20 + "Javapoint");  
+		System.out.println( "Javapoint" + (a+b) );
+		
+	      // Subtraction
+		System.out.println(20-10 + "Javapoint");
+		System.out.println( "Javapoint" + (20-10) );
+		
+		   // Multiplication
+		System.out.println(10*20 + "Javapoint");
+		System.out.println( "Javapoint" + 10*20 );
+		
+		  // Division
+		System.out.println(20/10 + "Javapoint");
+		System.out.println( "Javapoint" + 20/10);
+		
+	}
+
+}
